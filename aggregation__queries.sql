7871e148502b4f32e9950c8b6c52a84f26048607
Joao Paulo <jp.carvalhojardim1@gmail.com>
2026-03-23 14:37:38 +0100

    add count query

---
+++ aggregation__queries.sql
@@ -0,0 +1,15 @@
+--Select all products 
+SELECT COUNT(*) AS total_products
+FROM products_data_all;
+
+-- Select average price
+SELECT AVG(price) AS avg_price 
+FROM products_data_all
+
+--Select store name 
+SELECT name_store, COUNT (name) AS total_items 
+FROM products_data_all
+GROUP BY name_store;
+
+SELECT (*) FROM products_data_all;
+
---
+++ basic_queries.sql
@@ -0,0 +1,23 @@
+--Get all products 
+SELECT * FROM products_data_all;
+
+--Get products name and prices 
+SELECT name, price FROM products_data_all;
+
+--Get products name and categories 
+SELECT name, category FROM products_data_all;
+
+--
+SELECT name, price 
+
+FROM products_data_all
+
+WHERE price > 10;
+
+SELECT name, name_store 
+
+FROM products_data_all
+
+WHERE name_store = 'T-E-B';
+
+
